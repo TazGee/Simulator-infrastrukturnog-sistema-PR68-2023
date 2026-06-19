f2d0ac48e535b09e0ed9fe2ea07c8d477688c0e9
TazGee <nikolatasic2004@gmail.com>
2026-06-19 17:04:28 +0200

    Dodato dragovanje prozora

--- Presentation/AddEntityWindow.xaml.cs
+++ Presentation/AddEntityWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Presentation
 {
@@ -22,6 +13,36 @@ namespace Presentation
         public AddEntityWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowHeader_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+
+            if (IsInsideButton(e.OriginalSource as DependencyObject))
+            {
+                return;
+            }
+
+            DragMove();
+        }
+
+        private bool IsInsideButton(DependencyObject source)
+        {
+            while (source != null)
+            {
+                if (source is ButtonBase)
+                {
+                    return true;
+                }
+
+                source = VisualTreeHelper.GetParent(source);
+            }
+
+            return false;
         }
     }
 }
--- Presentation/LogWindow.xaml.cs
+++ Presentation/LogWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +23,36 @@ namespace Presentation
         public LogWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowHeader_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+
+            if (IsInsideButton(e.OriginalSource as DependencyObject))
+            {
+                return;
+            }
+
+            DragMove();
+        }
+
+        private bool IsInsideButton(DependencyObject source)
+        {
+            while (source != null)
+            {
+                if (source is ButtonBase)
+                {
+                    return true;
+                }
+
+                source = VisualTreeHelper.GetParent(source);
+            }
+
+            return false;
         }
     }
 }
